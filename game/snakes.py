29af1804cec3c6b55a9c12d2b9966cabbfe617a1
anselcheung <anselcheung@yahoo.com.sg>
2023-05-06 18:15:03 +0800

    Initial commit

---
+++ game/snakes.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+
+if __name__=='__main__':
+    pygame.init()
+
+    # create game canvas
+    surface = pygame.display.set_mode((500, 500))
+
+    # fill the background
+    surface.fill((0,0,0))
+    surface.display.flip()
+
+    
